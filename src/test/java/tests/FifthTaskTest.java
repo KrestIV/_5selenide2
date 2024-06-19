0ca3be39487384a4d09f20b1902f6c4f92898efd
Krest_IV <krestsov.iv@yandex.ru>
2024-06-19 22:27:51 +0200

    Added tests for 5th homework

--- src/test/java/tests/FifthTaskTest.java
+++ src/test/java/tests/FifthTaskTest.java
@@ -1,4 +1,30 @@
 package tests;
 
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class FifthTaskTest {
+
+    @Test
+    void goToSelenideAITest(){
+        open("https://github.com");
+        $$("button").findBy(text("Solutions")).hover();
+        $("a[href=\"https://github.com/enterprise\"]").click();
+        $("div.Primer_Brand__Stack-module__Stack___tASKe").shouldHave(text("The AI-powered " +
+        "developer platform"));
+    }
+
+    @Test
+    void dragNDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("div#column-a").dragAndDrop(to($("div#column-b")));
+        $("div#column-a").$(byText("B")).should(exist);
+        $("div#column-b").$(byText("A")).should(exist);
+    }
 }
